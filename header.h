fe02e7ba7b1e40783a8138d1f13a317ce0a8f4d6
Y4kuzi <support@provisionweb.org>
2019-10-14 22:51:36 +0200

    Snowflake 0.6

---
+++ header.h
@@ -0,0 +1,24 @@
+#ifndef _HEADER
+#define _HEADER
+
+#include <iostream>
+#include <netdb.h>
+#include <sstream>
+#include <boost/algorithm/string.hpp> // libboost-dev
+//#include <unistd.h>
+#include <vector>
+#include <map>
+#include <netinet/in.h> // for Android
+
+class User;
+class Channel;
+class Module;
+class Privmsg;
+class Notice;
+class Nick;
+class Kick;
+class Join;
+class Part;
+class Quit;
+
+#endif
